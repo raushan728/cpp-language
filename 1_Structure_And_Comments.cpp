164cbe745fe6961bd1a83f7c277f415431fbdce9
Rushan <raushansinghrajpoot687@gmail.com>
2025-05-30 19:46:56 +0530

    Structure And Comments

---
+++ 1_Structure_And_Comments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Single line comment
+
+/* Multi line comment
+Hello
+I
+am
+Raushan
+ */
+int main()
+{
+    int sum = 6;
+    cout << "Hello world " << sum;
+    return 0;
+}
