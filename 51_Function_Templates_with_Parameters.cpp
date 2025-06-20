a119bf78dbdb8db24726d9c9f1c55d00cf70c53e
Rushan <raushansinghrajpoot687@gmail.com>
2025-06-20 08:50:01 +0530

    Function-Templates with Parameters

---
+++ 51_Function_Templates_with_Parameters.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// float funcAverage(int a, int b){
+//     float avg= (a+b)/2.0;
+//     return avg;
+// }
+template <class T1, class T2>
+float funcAverage(T1 a, T2 b)
+{
+    float avg = (a + b) / 2.0;
+    return avg;
+}
+
+int main()
+{
+    // float a;
+    // a = funcAverage(5,2);
+    // printf("The average of these numbers is %f",a);
+    float a;
+    a = funcAverage(5, 2);
+    printf("The average of these numbers is %f", a);
+    return 0;
+}
